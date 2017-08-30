83bce247324dbe780bbc47582534d9d7b09c662e
Marc Rousavy <mrousavy@users.noreply.github.com>
2017-08-30 08:51:26 +0200

    Add XCode Library Project

---
+++ SwiftySockets/SwiftySockets.h
@@ -0,0 +1,13 @@
+//
+//  SwiftySockets.h
+//  SwiftySockets
+//
+//  Created by Marc Rousavy on 30/08/2017.
+//  Copyright © 2017 mrousavy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwiftySockets : NSObject
+
+@end
---
+++ SwiftySockets/SwiftySockets.m
@@ -0,0 +1,13 @@
+//
+//  SwiftySockets.m
+//  SwiftySockets
+//
+//  Created by Marc Rousavy on 30/08/2017.
+//  Copyright © 2017 mrousavy. All rights reserved.
+//
+
+#import "SwiftySockets.h"
+
+@implementation SwiftySockets
+
+@end
