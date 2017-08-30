8d1f0a3d5c7b4a6a4b130c6b1eca94f8127ab1f2
Marc Rousavy <mrousavy@users.noreply.github.com>
2017-08-30 09:11:35 +0200

    Add SwiftySocket static initializer

---
+++ SwiftySockets/SwiftySockets.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftySockets.swift
+//  SwiftySockets
+//
+//  Created by Marc Rousavy on 30/08/2017.
+//  Copyright © 2017 mrousavy. All rights reserved.
+//
+
+import Foundation
+
+
+func OpenNew(ip: String) -> SwiftySocket {
+    return SwiftySocket(ip: ip)
+}
